c4a079b2be788ccf5fa7a24124792ba0cca4e2d3
eorte052 <eorte052@ucr.edu>
2024-11-10 10:19:55 -0800

    moved test to correct folder

--- Tests/GUI_tests.py
+++ Tests/GUI_tests.py
@@ -1,14 +1,23 @@
 import unittest
 from PyQt5.QtWidgets import QApplication
-from ..GUI.main_window import MainWindow  
+from GUI.main_window import MainWindow
 
-app = QApplication([])
 
+app = QApplication.instance()
+
+#Testing class for Main Window Functionality
 class TestMainWindow(unittest.TestCase):
-    def startWindow(self):
+    def setUp(self):
+        """Set up the tests for Main Window"""
         self.window = MainWindow()
-    def closeWindow(self):
+
+
+    def close(self):
+        """Close the window after running all tests"""
         self.window.close()
+        self.window = None
+
+
 
 if __name__ == "__main__":
     unittest.main()
