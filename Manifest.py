5c2a475dcf5c9d79c37bdce0b8740b94d6f7c1c0
achan381 <114270759+leftwixx@users.noreply.github.com>
2024-11-18 03:48:54 +0000

    combined value and weight data container data

--- Manifest.py
+++ Manifest.py
@@ -1,12 +1,12 @@
 import numpy as np
+from ContainerData import ContainerData
 ## Manifest is the interface for interacting with the manifest file
 ## The manifest file is a grid of ContainerData objects
 class Manifest:
     def __init__(self, manifest_path, txtfile):
         self.manifest_path = manifest_path
         self.manifest_name = txtfile
-        self.WeightMatrix = []
-        self.ValueMatrix = []
+        self.ContainerMatrix = []
 
     ## read the manifest file at manifest_path
     ## and store the data in manifest for future use
@@ -15,35 +15,32 @@ class Manifest:
             List = [line.strip() for line in f]
         
         WeightList = []
-        ValueList = []
+        NameList = []
 
         for i in List:
             i = i[9:]
             WeightList.append(i[:7])
-            ValueList.append(i[9:])
+            NameList.append(i[9:])
         
-        WeightMatrix = []
-        ValueMatrix = []
+        ContainerMatrix = []
 
         for i in range(0, 8):
             WeightRow = WeightList[12 * i: (12 * (i + 1))]
-            WeightMatrix.append(WeightRow)
-            ValueRow = ValueList[12 * i: (12 * (i + 1))]
-            ValueMatrix.append(ValueRow)
-        
-        WeightMatrix = np.array(WeightMatrix, dtype='object')
-        ValueMatrix = np.array(ValueMatrix, dtype='object')
+            NameRow = NameList[12 * i: (12 * (i + 1))]
 
-        self.WeightMatrix = WeightMatrix
-        self.ValueMatrix = ValueMatrix
-        #WeightMatrix contains all the weights
-        #Value Matrix contains all the values, (container name, NAN, UNUSED)
-        return WeightMatrix, ValueMatrix
+            ContainerRow = []
+            for j in range(0,12):
+                c = ContainerData(NameRow[j], WeightRow[j])
+                ContainerRow.append(c)
+            
+            ContainerMatrix.append(ContainerRow)
+        
+        self.ContainerMatrix = ContainerMatrix
         pass
 
     ## Determine if a position is NAN
     def is_NAN(self, x, y):
-        if(ValueMatrix[x - 1, y - 1] == "NAN"):
+        if(self.ContainerMatrix[x - 1][y - 1].name == "NAN"):
             return True
         else:
             return False
@@ -51,7 +48,7 @@ class Manifest:
 
     ## Get the ContainerData at a position in the grid
     def data_at(self, x, y):
-        weight = self.WeightMatrix[x - 1, y - 1]
+        weight = self.ContainerMatrix[x - 1][y - 1].weight
         weight = weight[1:-1]
 
         while(weight[0] == "0"):
@@ -59,19 +56,20 @@ class Manifest:
 
         weight = int(weight)
 
-        return (self.ValueMatrix[x - 1, y - 1], weight)
+        container = ContainerData(self.ContainerMatrix[x - 1][y - 1].name, weight)
+        return container
         pass
 
     ## Set the ContainerData at a position in the grid
     def set_at(self, x, y, container_data):
-        self.ValueMatrix[x - 1, y - 1] = container_data[0]
+        self.ContainerMatrix[x - 1][y - 1].name = container_data.name
         
-        weight = str(container_data[1])
+        weight = str(container_data.weight)
         while(len(weight) < 5):
             weight = "0" + weight
         weight = '{' + weight + '}'
 
-        self.WeightMatrix[x - 1, y - 1] = weight
+        self.ContainerMatrix[x - 1][y - 1].weight = weight
         pass
 
     ## Save the edited manifest data to a new file
@@ -80,13 +78,13 @@ class Manifest:
         OutboundList = []
         position = ""
         weight = "{00000}"
-        value = "UNUSED"
+        name = "UNUSED"
         iterator = 0
         for x in range(1, 9):
             storex = x
             for y in range(1, 13):
-                weight = self.WeightMatrix[storex - 1][y - 1]
-                value = self.ValueMatrix[storex - 1][y - 1]
+                weight = self.ContainerMatrix[storex - 1][y - 1].weight
+                name = self.ContainerMatrix[storex - 1][y - 1].name
 
                 if(storex < 10):
                     x = "0" + str(storex)
@@ -96,7 +94,7 @@ class Manifest:
                 y = str(y)
 
                 position = "[" + x + ',' + y + "]"
-                OutboundList.append(position + ", " + weight + ", " + value + "\n")
+                OutboundList.append(position + ", " + weight + ", " + name + "\n")
 
         with open('./Manifests/' + self.manifest_name + 'OUTBOUND.txt', 'w') as f:
             for i in OutboundList:
@@ -108,20 +106,27 @@ path = "./Manifests/examplemanifest.txt" #change this line to input later
 txtfile = path.replace(".txt", "")
 txtfile = txtfile.replace("./Manifests/", "")
 
+#test declaration and read
 p = Manifest(path, txtfile);
 p.read_manifest()
 
-#print(p.WeightMatrix)
-#print(p.ValueMatrix)
+#test data at
+container = p.data_at(1,1)
+print("name: " , container.name)
+print("weight: ", container.weight)
 
-value, weight = p.data_at(1,1)
-print("value: " , value)
-print("weight: ", weight)
-
-newcontainer = ["John's shrimp and stuff" , 231]
+#test set at
+newcontainer = ContainerData("John's shrimp and stuff" , 231)
 p.set_at(1, 4, newcontainer)
-value, weight = p.data_at(1,4)
-print("value: " , value)
-print("weight: ", weight)
+container2 = p.data_at(1,4)
+print("name: " , container2.name)
+print("weight: ", container2.weight)
 
+#test is NAN
+if(p.is_NAN(1, 1)):
+    print("(1, 1) is NAN")
+if(p.is_NAN(1, 12)):
+    print("(1, 12) is NAN")
+
+#test save
 p.save()
