ef3132db8f8451c2c87577d243171655b528abe3
joey8angelo <joey8angelo@gmail.com>
2024-11-21 16:10:58 -0800

    slight modification to ContainerData and Move, add __str__ for printing

--- ContainerData.py
+++ ContainerData.py
@@ -1,5 +1,8 @@
 ## Data structure for container data
 class ContainerData:
-    def __init__(self, name, weight):
+    def __init__(self, name = "UNUSED", weight = "00000"):
         self.name = name
         self.weight = weight
+
+    def __str__(self):
+        return self.name + " {" + self.weight + "}"
--- Move.py
+++ Move.py
@@ -1,6 +1,11 @@
+from ContainerData import ContainerData
 # Data structure for move data
 class Move:
-    def __init__(self, m_t, m_f, ttm):
-        self.m_to = m_t
-        self.m_from = m_f
+    def __init__(self, m_from, m_to, ttm, container):
+        self.m_from = m_from
+        self.m_to = m_to
         self.time_to_move = ttm
+        self.container = container
+
+    def __str__(self):
+        return "Move " + str(self.container) + " from " + self.m_from + " to " + self.m_to + " in " + str(self.time_to_move) + " minutes"
